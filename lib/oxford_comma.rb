252e29afe1ac316e8fbb3982a966248ace872334
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-09 15:45:10 -0600

    commit tree 9ad1028f6eb2ab7113674b32049ea86cad2dc990

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+    if array.size == 1
+        array[0]
+    elsif array.size == 2
+        array.join(" and ")
+    elsif array.size > 2
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
+ end
 
-end
